78cd93446174db7988c39165e6d34e113d5cef61
Cheyenne Steskal <cheyenne.steskal@gmail.com>
2026-09-05 16:06:22 -0700

    start basic functionality

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <form @submit.prevent="addTask">
+    <tr>
+    <input type="text" maxlength="100" v-model="taskTitle" placeholder="What is there to do?"/>
+    <button type="submit">Add Task</button>
+  </tr>
+  </form>
+  <label>
+    <v-for tasks in localStorage>
+      
+    </v-for>
+  </label>
+</template>
+
+<script setup lang="ts">
+import {v4 as uuidv4 } from 'uuid'
+import {computed, onMounted, ref } from 'vue'
+
+// mounted: localstorage.get('tasks')
+// json parse
+// for task in tasks, populate li
+</script>
+
+
+<style scoped></style>
---
+++ src/Task.vue
@@ -0,0 +1,26 @@
+<template>
+<tr>
+    <h2>
+        {{ props.title  }}
+            <input type="checkbox" v-model="isDone"/>
+
+    </h2>
+</tr>
+</template>
+<script setup lang="ts">
+
+import { v4 } from 'uuid';
+import { ref } from 'vue';
+
+const props = defineProps({
+  title: String,
+  done: Boolean,
+  taskId: String
+})
+
+const isDone = ref(false)
+
+
+</script>
+<style scoped>
+</style>
