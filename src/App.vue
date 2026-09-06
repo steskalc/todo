1f8113de130a369678138081f2940cd2d15035e8
Cheyenne Steskal <cheyenne.steskal@gmail.com>
2026-09-06 11:11:44 -0700

    localstorage save works

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,66 @@
-
 <template>
   <form @submit.prevent="addTask">
-    <tr>
-    <input type="text" maxlength="100" v-model="taskTitle" placeholder="What is there to do?"/>
+    <input type="text" maxlength="100" v-model="taskTitle" placeholder="What is there to do?" />
     <button type="submit">Add Task</button>
-  </tr>
   </form>
-  <label>
-    <v-for tasks in localStorage>
-      
-    </v-for>
-  </label>
+  <li v-for="(storedTask, index) in tasks">
+    <TaskItem :title="storedTask.title" :done="storedTask.done" :id="storedTask.id" />
+    <button @click="removeTask(index)">X</button>
+  </li>
 </template>
 
 <script setup lang="ts">
-import {v4 as uuidv4 } from 'uuid'
-import {computed, onMounted, ref } from 'vue'
+import { v4 as uuidv4 } from 'uuid'
+import { onMounted, ref } from 'vue'
+import TaskItem from './TaskItem.vue'
 
-// mounted: localstorage.get('tasks')
-// json parse
-// for task in tasks, populate li
+interface task {
+  title: string,
+  done: boolean,
+  id: string
+}
+
+const tasks = ref<task[]>([])
+const taskTitle = ref<string>('')
+
+const addTask = () => {
+  const newTask = {
+    title: taskTitle.value,
+    done: false,
+    id: uuidv4()
+  }
+  tasks.value.push(newTask)
+  taskTitle.value = ''
+  save()
+}
+
+const removeTask = (index: number) => {
+  tasks.value.splice(index, 1)
+  save()
+}
+
+const save = () => {
+  let stringified = JSON.stringify(tasks.value)
+  localStorage.setItem('tasks', stringified)
+  console.log('---> saved: ', localStorage.getItem('tasks'))
+}
+
+onMounted(async () => {
+
+  let storedTasks = localStorage.getItem('tasks')
+
+  if (storedTasks) {
+    try {
+      console.log('---> stored tasks: ', storedTasks)
+      tasks.value.push(JSON.parse(storedTasks))
+    } catch (e) {
+      console.log(`Failed to parse stored tasks: ${e}`)
+    }
+  }
+
+})
+
+
 </script>
 
 
--- src/TaskItem.vue
+++ src/TaskItem.vue
@@ -1,26 +1,21 @@
 <template>
-<tr>
-    <h2>
-        {{ props.title  }}
-            <input type="checkbox" v-model="isDone"/>
-
-    </h2>
-</tr>
+  <h2>
+    {{ props.title }}
+    <input type="checkbox" v-model="isDone" />
+  </h2>
 </template>
 <script setup lang="ts">
 
-import { v4 } from 'uuid';
 import { ref } from 'vue';
 
 const props = defineProps({
   title: String,
   done: Boolean,
-  taskId: String
+  id: String
 })
 
 const isDone = ref(false)
 
 
 </script>
-<style scoped>
-</style>
+<style scoped></style>
